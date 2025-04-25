023abd31bc02914fd4f721bbf04b8595a9030e83
mhfashionhouse <baijidhossain2020@gmail.com>
2025-04-25 07:42:39 +0600

    Add files via upload

--- netlify/functions/conversion-api.js
+++ netlify/functions/conversion-api.js
@@ -8,7 +8,7 @@ function formatPhoneNumber(phone) {
   return phone.replace(/\D/g, '');
 }
 
-exports.handler = async function(event, context) {
+exports.handler = async (event) => {
   // Enable CORS
   if (event.httpMethod === 'OPTIONS') {
     return {
@@ -23,14 +23,15 @@ exports.handler = async function(event, context) {
   }
 
   if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: 'Method Not Allowed'
+    return { 
+      statusCode: 405, 
+      body: JSON.stringify({ error: 'Method Not Allowed' })
     };
   }
 
   try {
-    const { data, pixelId, accessToken } = JSON.parse(event.body);
+    const requestBody = JSON.parse(event.body);
+    const { data, pixelId, accessToken } = requestBody;
     
     if (!data || !pixelId || !accessToken) {
       throw new Error('Missing required parameters: data, pixelId, or accessToken');
@@ -47,30 +48,40 @@ exports.handler = async function(event, context) {
     const userAgent = event.headers['user-agent'] || '';
 
     // Format user data according to Facebook's requirements
-    const formattedData = await Promise.all(data.map(async event => ({
-      ...event,
-      event_id: event.event_id, // Include event_id for deduplication
-      event_time: event.event_time,
-      action_source: event.action_source,
-      event_source_url: event.event_source_url,
-      user_data: {
-        ...event.user_data,
-        em: event.user_data.em ? [await sha256(event.user_data.em.toLowerCase())] : undefined,
-        ph: event.user_data.ph ? [event.user_data.ph] : undefined,
-        external_id: event.user_data.external_id ? [event.user_data.external_id] : undefined,
-        client_ip_address: clientIp,
-        client_user_agent: userAgent,
-        fbc: event.user_data.fbc,
-        fbp: event.user_data.fbp
-      }
-    })));
+    const userData = {
+      ...data[0].user_data,
+      client_ip_address: clientIp,
+      client_user_agent: userAgent
+    };
 
     const url = `https://graph.facebook.com/v17.0/${pixelId}/events?access_token=${accessToken}`;
     
-    const response = await axios.post(url, {
-      data: formattedData,
-      test_event_code: process.env.FACEBOOK_TEST_EVENT_CODE // Optional: for testing
-    });
+    // Prepare the event data using the new structure
+    const eventData = {
+      data: [{
+        ...data[0],
+        user_data: userData
+      }]
+    };
+
+    // Remove undefined values recursively
+    const cleanObject = (obj) => {
+      Object.keys(obj).forEach(key => {
+        if (obj[key] && typeof obj[key] === 'object') {
+          cleanObject(obj[key]);
+        }
+        if (obj[key] === undefined || obj[key] === null) {
+          delete obj[key];
+        }
+      });
+      return obj;
+    };
+
+    const cleanEventData = cleanObject(eventData);
+    console.log('Sending event to Facebook:', JSON.stringify(cleanEventData, null, 2));
+
+    const response = await axios.post(url, cleanEventData);
+    console.log('Facebook API response:', response.data);
 
     return {
       statusCode: 200,
